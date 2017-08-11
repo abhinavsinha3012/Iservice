a31fae9ea1d596d90a0ad78851e268f4a4477498
Abhinav Sinha <30926798+abhinavsinha3012@users.noreply.github.com>
2017-08-11 16:38:36 +0530

    First code

---
+++ GitPro/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace GitPro.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ GitPro/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitPro.Startup))]
+namespace GitPro
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
